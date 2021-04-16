c668e46b47637e75d3614d350d8d2626a48440e6
ameliakrause47 <82601423+ameliakrause47@users.noreply.github.com>
2021-04-16 11:47:14 -0600

    Update miniGit.cpp
    added bool to check if file exists

--- miniGit.cpp
+++ miniGit.cpp
@@ -15,6 +15,22 @@
 
 using namespace std;
 
+    // i am thinking we run this function in the main to see if the file exsists alreay then we run the addFile function if the file does not exist in the directory
+    bool Git::checkFile(string fileName)
+    {
+    // check if file exists in directory 
+        for(int i =0; i< // number of entries in directory;)
+        {
+            if (fileName == files[i])
+            {
+                cout << "This file already exists please try again. "
+                cout << "Enter a file name: " << endl;
+                cin >> fileName;
+            } else {
+
+            }
+        }   
+    }
     void Git::addFile(string filename){
         // singlyNode * temp = new singlyNode;
         // temp->fileName = filename;
